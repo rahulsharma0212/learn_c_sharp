f395b424ccaf6402f80b7d4d48d7148b4112b411
Rahul Sharma <rahulsharma02121998@gmail.com>
2024-03-24 18:59:23 +0530

    variables finished

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,7 +6,40 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /******************
+            (1) Output in console
+            *******************/
+            Console.Write("Hey,");
+            Console.WriteLine("\tI likes\b c# \nso much");
+
+            /******************
+            (2) comments in c#
+            *******************/
+            // this is single line comment
+            /*
+                this is multi line comment
+             */
+
+            /******************
+            (3) variables
+            *******************/
+            int x; //declaration
+            x = 25; // initialization
+            int y = 35; // declaration + initialization
+            int z = x + y;
+            int age = 24;
+            double height = 254.8;
+            bool alive = true;
+            char symbol = '@';
+            string name = "Rahul Sharma";
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Your age is :: " + age);
+            Console.WriteLine("Your height is :: " + height + " cm");
+            Console.WriteLine("Are you Alive :: " + alive);
+            Console.WriteLine("Your symbol is :: " + symbol);
+            string userName = symbol + name;
+            Console.WriteLine("Your userName is :: " + userName);
+
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Run in visual Studio code
+
+- Run Command ```ctrl + shift + p``` and select ```Run code```
+
+---
+## Prequesites for vs Code
+- Install **code runner** extension
+- Install **c#** devkit extension
+- Install **dotnet6** or above
